61a199ffb6673fc6ef205b8d09688de3fa959d74
Touseef Liaqat <tliaqat@vmware.com>
2017-03-29 23:27:48 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Code
+
+This is template project for starting to Code in Java and practice algorithms.
+
+## Build Requirements
+
+ * JDK 1.8
+ * Maven
+
+## Test
+Use following command to build and run all tests.
+
+```
+mvn test
+```
---
+++ src/test/java/com/start/code/MainTest.java
@@ -0,0 +1,19 @@
+package com.start.code;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple Main.
+ */
+public class MainTest extends TestCase
+{
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
