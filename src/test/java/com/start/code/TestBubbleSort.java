d1440028990ecde0d69d968ff7d35420941bb1f4
Iqbal Anwer <ianwer99@yahoo.com>
2017-04-13 23:32:19 -0700

    Changed the test function names
    and removed loop from the empty array.

--- src/test/java/com/start/code/TestBubbleSort.java
+++ src/test/java/com/start/code/TestBubbleSort.java
@@ -6,7 +6,7 @@ import junit.framework.TestCase;
 
 public class TestBubbleSort extends TestCase {
 	
-	public void test0BubbleSort() { 
+	public void testBasic() { 
         int arr[] ={2,1};
         BubbleSort.SortMyArray(arr);//Sorting
         for(int i = 0; i < arr.length-1; i++){
@@ -14,7 +14,7 @@ public class TestBubbleSort extends TestCase {
         }
     } 
 	
-	public void test1BubbleSort() { 
+	public void testAlreadySorted() { 
         int arr[] ={1,2,3,4,5};
         BubbleSort.SortMyArray(arr);//Sorting
         for(int i = 0; i < arr.length-1; i++){
@@ -22,11 +22,9 @@ public class TestBubbleSort extends TestCase {
         }
     }
 	
-	public void test2BubbleSort() { 
+	public void testEmptyArray() { 
         int arr[] ={};
         BubbleSort.SortMyArray(arr);//Sorting
-        for(int i = 0; i < arr.length-1; i++){
-        	assertTrue(arr[i] <= arr[i+1]);
-        }
+        assertTrue(arr.length==0);
     }
 }
